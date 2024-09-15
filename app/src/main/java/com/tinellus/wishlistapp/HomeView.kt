33cd373941dc5fb21680ea52821617ad5d1336ef
gil-fullstack <gilservicosweb@gmail.com>
2024-09-15 11:42:19 -0300

    created wish data class and wishItem in HomeView

--- app/src/main/java/com/tinellus/wishlistapp/HomeView.kt
+++ app/src/main/java/com/tinellus/wishlistapp/HomeView.kt
@@ -3,10 +3,13 @@ package com.tinellus.wishlistapp
 import android.annotation.SuppressLint
 import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.LocalContentColor
 import androidx.compose.material.icons.Icons
@@ -22,36 +25,45 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.tinellus.wishlistapp.data.Wish
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomeView() {
-   val context = LocalContext.current
-   Scaffold(
-      topBar = { AppBarView(title = "WishList") {
-         Toast.makeText(context, "Button Clicked", Toast.LENGTH_LONG).show()
-      }
-      },
-      floatingActionButton = {
-         FloatingActionButton(
-            modifier = Modifier.padding(all = 28.dp),
-            contentColor = Color.White,
-            backgroundColor = Color.Black,
-            onClick = { /*TODO*/ }) 
-         {
-            Icon(imageVector = Icons.Default.Add, contentDescription = null, tint = LocalContentColor.current)
-         }
-      },
-      modifier = Modifier.padding(top = 20.dp)
-   ) {
-      LazyColumn(modifier = Modifier
-         .fillMaxSize()
-         .padding(it)) {
+    val context = LocalContext.current
+    Scaffold(
+        topBar = {
+            AppBarView(title = "WishList") {
+                Toast.makeText(context, "Button Clicked", Toast.LENGTH_LONG).show()
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(all = 28.dp),
+                contentColor = Color.White,
+                backgroundColor = Color.Black,
+                onClick = { /*TODO*/ })
+            {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = null,
+                    tint = LocalContentColor.current
+                )
+            }
+        },
+        modifier = Modifier.padding(top = 20.dp)
+    ) {
+        LazyColumn(
+            modifier = Modifier
+               .fillMaxSize()
+               .padding(it)
+        ) {
 
-      }
-   }
+        }
+    }
 
 //   Scaffold(
 //     topBar = {
@@ -71,4 +83,22 @@ fun HomeView() {
 //   ) {
 //
 //   }
+}
+
+@Composable
+fun WishItem(wish: Wish, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+           .fillMaxWidth()
+           .padding(start = 8.dp, top = 8.dp, end = 8.dp)
+           .clickable { onClick() },
+        elevation = 10.dp,
+        backgroundColor = Color.White
+    ) {
+       Column(modifier = Modifier.padding(16.dp)) {
+          Text(text = wish.title, fontWeight = FontWeight.ExtraBold)
+          Text(text = wish.description,)
+       }
+
+    }
 }
---
+++ app/src/main/java/com/tinellus/wishlistapp/data/Wish.kt
@@ -0,0 +1,7 @@
+package com.tinellus.wishlistapp.data
+
+data class Wish(
+    val id:Int = 0,
+    val title:String = "",
+    val description:String =""
+)
