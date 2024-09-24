288bc435c18e3f907b6a1ff731203fe57a1c50e4
gil-fullstack <gilservicosweb@gmail.com>
2024-09-24 14:20:48 -0300

    created a WishRepository

--- app/src/main/java/com/tinellus/wishlistapp/WishViewModel.kt
+++ app/src/main/java/com/tinellus/wishlistapp/WishViewModel.kt
@@ -4,8 +4,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.tinellus.wishlistapp.data.Wish
+import com.tinellus.wishlistapp.data.WishRepository
+import kotlinx.coroutines.flow.Flow
 
-class WishViewModel:ViewModel() {
+class WishViewModel(
+    private val wishRepository: WishRepository
+):ViewModel() {
 
     var wishTitleState by mutableStateOf("")
     var wishDescription by mutableStateOf("")
@@ -17,4 +22,6 @@ class WishViewModel:ViewModel() {
     fun onDescriptionState(newString:String){
         wishDescription = newString
     }
+
+    lateinit var getAllWishes: Flow<List<Wish>>
 }
---
+++ app/src/main/java/com/tinellus/wishlistapp/data/WishRepository.kt
@@ -0,0 +1,25 @@
+package com.tinellus.wishlistapp.data
+
+import kotlinx.coroutines.flow.Flow
+
+class WishRepository(private val wishDao: WishDao) {
+
+    fun addAWish(wish: Wish){
+        wishDao.addWish(wish)
+    }
+
+    fun getWishes(): Flow<List<Wish>> = wishDao.getAllWishes()
+
+    fun getAWish(id:Long): Flow<Wish> {
+        return wishDao.getAllWishesById(id)
+    }
+
+    suspend fun updateAWish(wish: Wish){
+        wishDao.updateAWish(wish)
+    }
+
+    suspend fun deleteAWish(wish: Wish){
+        wishDao.deleteAWish(wish)
+    }
+
+}
