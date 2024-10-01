7489651a6c0e6c52077434c19ed79de25c52142a
gil-fullstack <gilservicosweb@gmail.com>
2024-10-01 14:55:04 -0300

    update AddDetailViewModel

--- app/src/main/java/com/tinellus/wishlistapp/AddEditDetailView.kt
+++ app/src/main/java/com/tinellus/wishlistapp/AddEditDetailView.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 
@@ -17,6 +18,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,6 +32,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.tinellus.wishlistapp.data.Wish
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -36,6 +41,12 @@ fun AddEditDetailView(
     viewModel: WishViewModel,
     navController: NavController
 ){
+    val snackMessage = remember {
+        mutableStateOf("")
+    }
+    val scope = rememberCoroutineScope()
+    val scaffoldState = rememberScaffoldState()
+
     Scaffold(
         topBar = { AppBarView(title = 
         if(id != 0L) stringResource(id = R.string.update_wish)
@@ -65,9 +76,19 @@ fun AddEditDetailView(
             Spacer(modifier = Modifier.height(10.dp))
             Button(onClick = {
                 if(viewModel.wishTitleState.isNotEmpty() && viewModel.wishDescription.isNotEmpty()){
-                    //TODO
+                    if(id != 0L){
+                        // TODO Update wish
+                    }else{
+                        viewModel.addWish(
+                            Wish(
+                                title = viewModel.wishTitleState.trim(),
+                                description = viewModel.wishDescription.trim()
+                            )
+                        )
+                        snackMessage.value = "Wish has been created"
+                    }
                 }else{
-                    // TODO
+                    snackMessage.value = "Enter fields to create a wish "
                 }
             }) {
                 Text(
